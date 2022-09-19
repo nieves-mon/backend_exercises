131b466d37a2ab6e9e0a098ec04e753d97a48183
nieves-mon <cunanan.nm@gmail.com>
2022-09-19 21:28:56 +0800

    Add each version to item#4

--- activity_2/act_2.rb
+++ activity_2/act_2.rb
@@ -76,5 +76,12 @@ arr = [6, 3, 1, 8, 4, 2, 10, 65, 102]
 
 puts "\nthis is my solution to item #4"
 
+# each version
+# new_arr = []
+# arr.each {|num| new_arr << num unless num % 2 != 0}
+
+# select/filter version
+# new_arr = arr.filter {|num| num % 2 == 0}
 new_arr = arr.select {|num| num % 2 == 0}
+
 puts "Numbers divisible by 2: #{new_arr}"
