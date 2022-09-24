d5baea079adab145adf9d034500f123f98ffdccc
nieves-mon <cunanan.nm@gmail.com>
2022-09-24 11:03:18 +0800

    Change

--- activity_5/act_5_part2.rb
+++ activity_5/act_5_part2.rb
@@ -17,23 +17,34 @@ class User
         @ip_address = ip_address
     end
 
-    def login
-      puts "User logged in. IP address: #{@ip_address}"
-    end
-
     def change_password=(new_password)
         @password = new_password
+        puts "Successfully changed password"
+    end
+
+    protected
+
+    def login
+      puts "User logged in. IP address: #{@ip_address}"
     end
 end
 
 
 class Admin < User
     include AdminPermisson
+
+    def admin_login
+        login
+    end
 end
 
 
 class Buyer < User
     include BuyerPermission
+
+    def buyer_login
+        login
+    end
 end
 
 
@@ -41,13 +52,13 @@ end
 ## execute
 
 my_admin = Admin.new('avionuser', 'password', '127.0.0.1')
-my_admin.login
+my_admin.admin_login
 my_admin.edit_users_profile
 
 my_admin.change_password = 'new_password'
 
 buyer = Buyer.new('juan', 'password', '127.0.0.1')
-buyer.login
+buyer.buyer_login
 buyer.buy
 
 buyer.change_password = 'new_password'
